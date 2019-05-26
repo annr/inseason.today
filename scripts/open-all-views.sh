eb34cec0d6a269197db77e4d0e62060b5b0385be
Ann Robson <nosbora@gmail.com>
2019-05-26 09:53:59 -0700

    add a script that opens all views for the year, for testing

---
+++ scripts/open-all-views.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# run like: ./scripts/open-all-views.sh 
+
+#for (var i = 1; i < 13; i++) {
+#  console.log('open https://inseason.today/?testDate=' + i + '/01/2018');
+#}
+
+open https://inseason.today/?testDate=1/01/2018
+open https://inseason.today/?testDate=2/01/2018
+open https://inseason.today/?testDate=3/01/2018
+open https://inseason.today/?testDate=4/01/2018
+open https://inseason.today/?testDate=5/01/2018
+open https://inseason.today/?testDate=6/01/2018
+open https://inseason.today/?testDate=7/01/2018
+open https://inseason.today/?testDate=8/01/2018
+open https://inseason.today/?testDate=9/01/2018
+open https://inseason.today/?testDate=10/01/2018
+open https://inseason.today/?testDate=11/01/2018
+open https://inseason.today/?testDate=12/01/2018
